909d55db87b5bfecb34563164d17b30d3bfdad10
Jamile Lima Leite <jamile.lima-leite@etu.univ-grenoble-alpes.fr>
2022-11-26 18:22:44 +0100

    Producteurs et Consommateurs définis comme threads pour interagir avec buffer ProdConsBuffer

--- Projet_ProdCons/src/prodcons/Consommateur.java
+++ Projet_ProdCons/src/prodcons/Consommateur.java
@@ -1,10 +1,19 @@
 package prodcons;
 
-public class Consommateur {
+public class Consommateur extends Thread{
 	ProdConsBuffer pcbuffer;
+	Message msg;
 
 	public Consommateur(ProdConsBuffer pcbuffer) {
 		this.pcbuffer = pcbuffer;
+		start();
 	}
 	
+	public void run() {
+		try {
+			msg = pcbuffer.get();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
 }
--- Projet_ProdCons/src/prodcons/Producteur.java
+++ Projet_ProdCons/src/prodcons/Producteur.java
@@ -1,11 +1,20 @@
 package prodcons;
 
-public class Producteur {
+public class Producteur extends Thread{
 	ProdConsBuffer pcbuffer;
+	Message msg;
 
-	public Producteur(ProdConsBuffer pcbuffer) {
+	public Producteur(ProdConsBuffer pcbuffer, Message msg) {
 		this.pcbuffer = pcbuffer;
+		this.msg = msg;
+		start();
 	}
 	
-	
+	public void run() {
+		try {
+			pcbuffer.put(msg);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
 }
