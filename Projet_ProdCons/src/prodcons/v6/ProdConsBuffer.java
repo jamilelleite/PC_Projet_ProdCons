022f4103f8f8c8164a0580f8556b0eed618374bf
Jamile Leite <jamilkshake@gmail.com>
2022-12-06 17:13:32 -0300

    Question 6 - put() method with n places in buffer

--- Projet_ProdCons/src/prodcons/v6/ProdConsBuffer.java
+++ Projet_ProdCons/src/prodcons/v6/ProdConsBuffer.java
@@ -1,5 +1,6 @@
 package prodcons.v6;
 
+import java.util.concurrent.Semaphore;
 
 public class ProdConsBuffer implements IProdConsBuffer{
 
@@ -9,12 +10,15 @@ public class ProdConsBuffer implements IProdConsBuffer{
 	int nempty;
 	int in = 0;
 	int out = 0;
+	Semaphore fifo;
+	//Semaphore rdv;
 	
 	public ProdConsBuffer(int bufferSz) {
 		this.bufferSz = bufferSz;
 		this.buffer = new Message[bufferSz];
 		this.nfull = bufferSz;
 		this.nempty = 0;
+		Semaphore fifo = new Semaphore(1);
 	}
 
 	
@@ -29,60 +33,32 @@ public class ProdConsBuffer implements IProdConsBuffer{
 		notifyAll();		
 	}
 	
-	public synchronized void put (Message m, int n) throws InterruptedException{
-		//TO DO
-		while (nfull < n) {
-			wait();
-		}
-		//Putting the message m, n times in the buffer
-		for(int i = 0; i<n; i++) {
-			buffer[in] = m;
-			in = (in + 1) % bufferSz;
-			nempty++;
-			nfull--;
-		}
-		//The producer waiting as long as all the iterations of his message isn't out completely
-		for(int i = 0; i<bufferSz; i++) {
-			if(buffer[i] == m)
-				wait();
-		}
-		notifyAll();
-	}
-	//Modifying get method so that if a consumer takes a message, he can finish only if all it's iterations
-	//are out of the buffer
-	public synchronized Message get() throws InterruptedException {
-		while (nempty == 0) {
-			wait();
-		}
-		Message msg = buffer[out];
-		buffer[out] = null; //freeing the spot of the taken message
-		out = (out + 1) % bufferSz;
-		nempty--;
-		nfull++;
-		//Waiting as long as there is the same message in the buffer
-		for(int i = 0; i<bufferSz; i++) {
-			if(buffer[i] == msg) {
-				wait();
+	public void put (Message m, int n) throws InterruptedException{
+		int putCounter = 0;
+		//rdv = new Semaphore(n);
+		fifo.acquire();
+		synchronized(this){
+			while(putCounter < n){
+				while(nfull == 0){
+					wait();
+				}
+				buffer[in] = msg;
+				in = (in + 1) % bufferSz;
+				nempty++;
+				nfull--;
+				putCounter++;
 			}
 		}
-		notifyAll();
-		return msg;
+		notifyAll;
+		fifo.release();
+	}
+
+	public synchronized Message get() throws InterruptedException {
+
 	}
 	
 	public synchronized Message[] get(int k) throws InterruptedException{
-		while (nempty < k) {
-			wait();
-		}
-		Message[] M = new Message[k];
-		for(int i = 0; i<k; i++) {
-			Message msg = buffer[out];
-			M[i] = msg;
-			out = (out + 1) % bufferSz;
-		}
-		nempty = nempty - k;
-		nfull = nfull + k;
-		notifyAll();
-		return M;
+
 	}
 	
 	public int nmsg() {
