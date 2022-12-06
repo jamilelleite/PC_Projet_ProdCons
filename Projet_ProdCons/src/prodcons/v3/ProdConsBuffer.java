69f585c9466f4c9507be4585788289070df496bb
Jamile Leite <jamilkshake@gmail.com>
2022-12-06 10:11:16 -0300

    Synchronized function instead of 2 semaphores for keeping index incrementation exclusives

--- Projet_ProdCons/src/prodcons/v3/ProdConsBuffer.java
+++ Projet_ProdCons/src/prodcons/v3/ProdConsBuffer.java
@@ -8,7 +8,6 @@ public class ProdConsBuffer implements IProdConsBuffer{
 	Message buffer[];
 	Semaphore notEmpty;
 	Semaphore notFull;
-	Semaphore Mutex1, Mutex2;
 	int in = 0;
 	int out = 0;
 	
@@ -17,17 +16,15 @@ public class ProdConsBuffer implements IProdConsBuffer{
 		this.buffer = new Message[bufferSz];
 		notEmpty = new Semaphore(bufferSz);
 		notFull = new Semaphore(0);
-		Mutex1 = new Semaphore(1);
-		Mutex2 = new Semaphore(1);
 	}
 
 	
 	public void put(Message msg) throws InterruptedException {
 		notFull.acquire();
-		Mutex1.acquire();
-		buffer[in] = msg;
-		in = (in + 1) % bufferSz;
-		Mutex1.release();
+		synchronized(this){
+			buffer[in] = msg;
+			in = (in + 1) % bufferSz;
+		}
 		notifyAll();
 		notEmpty.release();
 	}
@@ -35,10 +32,10 @@ public class ProdConsBuffer implements IProdConsBuffer{
 	
 	public Message get() throws InterruptedException {
 		notEmpty.acquire();
-		Mutex2.acquire();
-		Message msg = buffer[out];
-		out = (out + 1) % bufferSz;
-		Mutex2.release();
+		synchronized(this){
+			Message msg = buffer[out];
+			out = (out + 1) % bufferSz;
+		}
 		notifyAll();
 		notFull.release();
 		return msg;
